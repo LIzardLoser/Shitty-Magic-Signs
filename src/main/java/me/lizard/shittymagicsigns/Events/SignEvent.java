ae0c48893bb771f0b53e22d943db5bb582eacd49
BuildTools <unconfigured@null.spigotmc.org>
2022-04-24 19:25:16 -0400

    made shit happen idk

--- src/main/java/me/lizard/shittymagicsigns/Events/SignEvent.java
+++ src/main/java/me/lizard/shittymagicsigns/Events/SignEvent.java
@@ -6,6 +6,7 @@ import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -20,68 +21,73 @@ public class SignEvent implements Listener {
     @EventHandler
     public void onSignChangeEvent(SignChangeEvent e){
 
+        Player p = e.getPlayer();
+
         var mss = e.getLine(1).equalsIgnoreCase("Magic Sign");//mss = magic sign string
         var gl = e.getLine(2);// gl == get line
 
+        if (p.isOp() || p.hasPermission("shittymagicsigns.sms")){
 
+            if (mss && gl.equalsIgnoreCase("Diamond Block")){// diamond block
 
-        if (mss && gl.equalsIgnoreCase("Diamond Block")){// diamond block
+                e.getBlock().breakNaturally();
+                e.getBlock().setType(Material.DIAMOND_BLOCK);
 
-            e.getBlock().breakNaturally();
-            e.getBlock().setType(Material.DIAMOND_BLOCK);
+            }else if (mss && gl.equalsIgnoreCase("Gold Block")){//gold block
 
-        }else if (mss && gl.equalsIgnoreCase("Gold Block")){//gold block
+                e.getBlock().breakNaturally();
+                e.getBlock().setType(Material.GOLD_BLOCK);
 
-            e.getBlock().breakNaturally();
-            e.getBlock().setType(Material.GOLD_BLOCK);
+            }else if(mss && gl.equalsIgnoreCase("Gold")){//gold ingot
 
-        }else if(mss && gl.equalsIgnoreCase("Gold")){//gold ingot
+                e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.GOLD_INGOT));
+                e.getBlock().breakNaturally();
 
-            e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.GOLD_INGOT));
-            e.getBlock().breakNaturally();
+            }else if(mss && gl.equalsIgnoreCase("Diamond")){//diamond
 
-        }else if(mss && gl.equalsIgnoreCase("Diamond")){//diamond
+                e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.DIAMOND));
+                e.getBlock().breakNaturally();
 
-            e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.DIAMOND));
-            e.getBlock().breakNaturally();
+            }else if(mss && gl.equalsIgnoreCase("Zombie")){
 
-        }else if(mss && gl.equalsIgnoreCase("Zombie")){
+                e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.ZOMBIE);
+                e.getBlock().breakNaturally();
 
-            e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.ZOMBIE);
-            e.getBlock().breakNaturally();
+            }else if(mss && gl.equalsIgnoreCase("Creeper")){
 
-        }else if(mss && gl.equalsIgnoreCase("Creeper")){
+                e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.CREEPER);
+                e.getBlock().breakNaturally();
 
-            e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.CREEPER);
-            e.getBlock().breakNaturally();
+            }else if(mss && gl.equalsIgnoreCase("Charged creeper") || mss & e.getLine(2).equalsIgnoreCase("CC")){
 
-        }else if(mss && gl.equalsIgnoreCase("Charged creeper") || mss & e.getLine(2).equalsIgnoreCase("CC")){
+                Creeper cc = (Creeper)e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.CREEPER);
+                cc.setPowered(true);
+                e.getBlock().breakNaturally();
 
-            Creeper cc = (Creeper)e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.CREEPER);
-            cc.setPowered(true);
-            e.getBlock().breakNaturally();
+            }else if(mss && gl.equalsIgnoreCase("Skeleton")){
 
-        }else if(mss && gl.equalsIgnoreCase("Skeleton")){
+                e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.SKELETON);
+                e.getBlock().breakNaturally();
 
-            e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.SKELETON);
-            e.getBlock().breakNaturally();
+            }else if (mss && gl.equalsIgnoreCase("God Sword")){
 
-        }else if (mss && gl.equalsIgnoreCase("God Sword")){
+                ItemStack sword = new ItemStack(Material.NETHERITE_SWORD);
+                ItemMeta swordM = sword.getItemMeta();
+                swordM.addEnchant(Enchantment.DAMAGE_ALL, 5000, true);
+                swordM.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, new AttributeModifier("attack speed", 0.2, AttributeModifier.Operation.ADD_NUMBER));
+                swordM.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier("attack damage", 1500.0, AttributeModifier.Operation.ADD_NUMBER));
+                sword.setItemMeta(swordM);
+                e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), sword);
+                e.getBlock().breakNaturally();
 
-            ItemStack sword = new ItemStack(Material.NETHERITE_SWORD);
-            ItemMeta swordM = sword.getItemMeta();
-            swordM.addEnchant(Enchantment.DAMAGE_ALL, 5000, true);
-            swordM.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED, new AttributeModifier("attack speed", 0.2, AttributeModifier.Operation.ADD_NUMBER));
-            swordM.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier("attack damage", 1500.0, AttributeModifier.Operation.ADD_NUMBER));
-            sword.setItemMeta(swordM);
-            e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), sword);
-            e.getBlock().breakNaturally();
+            }else if (mss && gl.equalsIgnoreCase("spider")){
 
-        }else if (mss && gl.equalsIgnoreCase("spider")){
+                e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.SPIDER);
+                e.getBlock().breakNaturally();
 
-            e.getBlock().getWorld().spawnEntity(e.getBlock().getLocation(), EntityType.SPIDER);
-            e.getBlock().breakNaturally();
+            }
 
         }
+
     }
 }
